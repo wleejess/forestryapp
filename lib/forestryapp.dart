939a36b1cff9ad173712c8f69ce30d7a87ffd3aa
Jeremy Tsang <jeremytsang1@gmail.com>
2024-01-24 19:12:04 -0800

    Create Material App in order to Material UI benefits
    Get access to text directionality, routing, app title, etc.

--- lib/forestryapp.dart
+++ lib/forestryapp.dart
@@ -1,10 +1,15 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
 
 class ForestryApp extends StatelessWidget {
   const ForestryApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(
+      title: 'Forestry Wellness Checkup App',
+      // Replace `Placeholder` with screen widgets while testing until
+      // navigation implemented.
+      home: Placeholder(),
+    );
   }
 }
