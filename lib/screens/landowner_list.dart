997dea8658ba8fff9b16865e62e1379286972668
Jeremy Tsang <jeremytsang1@gmail.com>
2024-01-24 19:19:52 -0800

    Create empty screen to test `ForestryScaffold`

---
+++ lib/screens/landowner_list.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+import "package:forestryapp/components/forestry_scaffold.dart";
+
+class LandownerList extends StatelessWidget {
+  // Instance variables ////////////////////////////////////////////////////////
+
+  // Constructors //////////////////////////////////////////////////////////////
+  const LandownerList({super.key});
+
+  // Methods ///////////////////////////////////////////////////////////////////
+  @override
+  Widget build(BuildContext context) {
+    return const ForestryScaffold(title: "Landowners", body: Placeholder());
+  }
+}
