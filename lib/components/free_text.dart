c21860f66d16ec7063cd8ed92518e7bf0c2be4f9
Jessica Lee <wlee.jessica@gmail.com>
2024-01-25 16:09:52 -0800

    Initial commit of free_text widget, and 'other_issues' screen.

---
+++ lib/components/free_text.dart
@@ -0,0 +1,26 @@
+import "package:flutter/material.dart";
+
+/// Widget for entering free text, can be reused in different pages.
+
+class FreeTextBox extends StatelessWidget {
+  final TextEditingController controller;
+  final String hintText;
+  final void Function(String) onChanged;
+
+  FreeTextBox({
+    required this.controller,
+    required this.hintText,
+    required this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        hintText: hintText, // helper text placeholders
+      ),
+      onChanged: onChanged,
+    );
+  }
+}
---
+++ lib/screens/other_issues.dart
@@ -0,0 +1,35 @@
+import "package:flutter/material.dart";
+import "package:forestryapp/components/fab_creation.dart";
+import "package:forestryapp/components/forestry_scaffold.dart";
+import "package:forestryapp/components/model_list_view.dart";
+import "package:forestryapp/components/free_text.dart";
+
+class OtherIssues extends StatelessWidget {
+  // Instance Variables
+  final TextEditingController _textController = TextEditingController();
+  final _title = "Forest Wellness Checkup";
+  @override
+  Widget build(BuildContext context) {
+      return ForestryScaffold(
+        title: _title,
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text('Other Issues',
+              ),
+
+              SizedBox(height: 16.0),
+
+              FreeTextBox(
+                controller: _textController, 
+                hintText: 'Describe any other health related issues you observed.', 
+                onChanged:
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
