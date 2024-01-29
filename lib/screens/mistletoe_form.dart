d60f92bd94da42979528c09dc07e5e3d478bff94
Anastasia Sledgianowski <ani.sledge@gmail.com>
2024-01-29 00:22:42 -0700

    Adds a TextField for the mistletoe location.

--- lib/screens/mistletoe_form.dart
+++ lib/screens/mistletoe_form.dart
@@ -8,8 +8,10 @@ class MistletoeForm extends StatelessWidget {
   static const _title = "Mistletoe Infections";
   static const _buttonLabelPrev = "Previous";
   static const _buttonLabelNext = "Next";
+  static const _locationHeading = "Mistletoe location";
 
   static const double _heightButton = 0.05;
+  static const double _heightHeading = 0.04;
 
   // Constructor //////////////////////////////////////////
   // Creates a screen with a form to enter data on mistletoe infections.
@@ -24,10 +26,28 @@ class MistletoeForm extends StatelessWidget {
       title: _title, 
       body: Column(
         children: <Widget>[
+          _buildLocationHeading(context),
+          TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Enter the mistletoe location(s)',
+            ),
+          ),
         ],
       ),
     );
   }
 
-
+  Widget _buildLocationHeading(BuildContext context) {
+    return RelativeSizedBox(
+      percentageHeight: _heightHeading,
+      child: Container(
+        alignment: Alignment.center,
+        child: Text(
+          _locationHeading,
+          style: Theme.of(context).textTheme.headlineMedium,
+        ),
+      ),
+    );
+  }
 }
