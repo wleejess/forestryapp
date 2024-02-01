b52d7d6a877ef97c929edd428eabbd6c3380dca9
Jeremy Tsang <jeremytsang1@gmail.com>
2024-02-01 15:25:23 -0800

    Make general layout for Area Review screen
    Layout headings, scrollable list, and bottom row buttons.

--- lib/screens/area_review.dart
+++ lib/screens/area_review.dart
@@ -16,7 +16,52 @@ class AreaReview extends StatelessWidget {
   Widget build(BuildContext context) {
     return ForestryScaffold(
       title: "$_titlePrefix: $_area",
-      body: const Placeholder(),
+      body: Column(
+        children: [
+          _buildAreaNameHeading(context),
+          Expanded(child: _buildAreaPropertiesListView(context)),
+          _buildButtonRow(context),
+        ],
+      ),
+    );
+  }
+
+  // Heading ///////////////////////////////////////////////////////////////////
+  Widget _buildAreaNameHeading(BuildContext context) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      child: Text(_area, style: Theme.of(context).textTheme.headlineMedium),
+    );
+  }
+
+  // Area Properties ///////////////////////////////////////////////////////////
+  Widget _buildAreaPropertiesListView(BuildContext context) {
+    final identicalItemInList = SizedBox(
+      height: 100,
+      child: Container(
+        color: Colors.green,
+        margin: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+      ),
+    );
+
+    return ListView.builder(
+      itemCount: 20,
+      itemBuilder: (BuildContext context, int i) {
+        return identicalItemInList;
+      },
+    );
+  }
+
+  // Buttons ///////////////////////////////////////////////////////////////////
+  _buildButtonRow(BuildContext context) {
+    return Row(
+      children: [
+        OutlinedButton(onPressed: () {}, child: const Text("Generate PDF")),
+        OutlinedButton(onPressed: () {}, child: const Text("Send Email")),
+        Expanded(child: Container()),
+        OutlinedButton(onPressed: () {}, child: const Text("Edit")),
+        OutlinedButton(onPressed: () {}, child: const Text("Delete")),
+      ],
     );
   }
 }
