4288f31863f5defe4c460d7089edc3df0540a3b0
Anastasia Sledgianowski <ani.sledge@gmail.com>
2024-02-02 01:07:42 -0700

    Add variables containing the heading, label, and option text for the mistletoe form fields.

--- lib/screens/mistletoe_screen.dart
+++ lib/screens/mistletoe_screen.dart
@@ -5,6 +5,23 @@ import "package:forestryapp/components/radio_options.dart";
 class MistletoeScreen extends StatelessWidget {
   // Static variables //////////////////////////////////////////////////////////
   static const _title = "Mistletoe Infections";
+  static const _uniformityHeading = "Uniformity";
+  static const _uniformityOptions = ['Uniform throughout stand', 'Spotty'];
+  static const _uniformityDescription = 
+    "Are the mistletoe infections isolated/grouped, or uniform throughout the area?";
+  static const _locationHeading = "Mistletoe location";
+  static const _locationDescription = 
+    "If the mistletoe infections are spotty, record their locations.";
+  static const _hawksworthHeading = "Hawksworth infection rating";
+  static const _hawksworthOptions = ['none', 'low', 'medium', 'high'];
+  static const _hawksworthDescription = 
+    "Rate the mistletoe infection level and check the appropriate rating. If you "
+    "are not familiar with this rating system, write your observations elsewhere, "
+    "such as under 'Diagnosis & Suggestions.'";
+  static const _speciesHeading = "Tree species infected";
+  static const _speciesDescription = "List the tree species infected with mistletoe.";
+  static const _buttonLabelPrevious = "Previous";
+  static const _buttonLabelNext = "Next";
 
   // Constructor ///////////////////////////////////////////////////////////////
   /// Creates a screen to enter information on mistletoe infections.
