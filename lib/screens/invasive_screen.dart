104fc721ec6214cd63d09f89b228eb09c3daf58d
Anastasia Sledgianowski <ani.sledge@gmail.com>
2024-02-03 23:45:09 -0700

    Add screen for invasive plants and animals and wildlife, based on pests screen.

---
+++ lib/screens/invasive_screen.dart
@@ -0,0 +1,87 @@
+import "package:flutter/material.dart";
+import "package:forestryapp/components/forestry_scaffold.dart";
+
+class InvasiveScreen extends StatelessWidget {
+  // Static variables //////////////////////////////////////////////////////////
+  static const _title = "Pests & Damages B";
+  static const _invasiveHeading = "Invasive plants & animals";
+  static const _invasiveDescription = "List any invasive plants or animals observed.";
+  static const _wildlifeHeading = "Wildlife damage/issues";
+  static const _wildlifeDescription = "Describe wildlife damage to tree seedlings, "
+    "saplings or mature trees. If regeneration is damaged, estimate the percentage "
+    "of seedlings/saplings affected.";
+  static const _buttonLabelPrevious = "Previous";
+  static const _buttonLabelNext = "Next";
+
+  // Constructor ///////////////////////////////////////////////////////////////
+  /// Creates a screen to enter information on invasive plants and animals,
+  /// and wildlife damage present in the area.
+  const InvasiveScreen({super.key});
+
+  // Methods ///////////////////////////////////////////////////////////////////
+  @override
+  Widget build(BuildContext context) {
+    return ForestryScaffold(
+      title: _title,
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Wrap(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: _buildInvasiveInput(context),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: _buildWildlifeInput(context),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _buildButtonPrevious(context),
+                _buildButtonNext(context),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Inputs ////////////////////////////////////////////////////////////////////
+  Widget _buildTextInput(BuildContext context, label, helper) {
+    return TextField(
+      decoration: InputDecoration(
+        labelText: label,
+        helperText: helper,
+        border: const OutlineInputBorder(),
+      )
+    );
+  }
+
+  Widget _buildInvasiveInput(BuildContext context) {
+    return _buildTextInput(context, _invasiveHeading, _invasiveDescription);
+  }
+
+  Widget _buildWildlifeInput(BuildContext context) {
+    return _buildTextInput(context, _wildlifeHeading, _wildlifeDescription);
+  }
+
+  // Buttons ///////////////////////////////////////////////////////////////////
+  Widget _buildButtonPrevious(BuildContext context) {
+    return OutlinedButton(
+      onPressed: () {},
+      child: const Text(_buttonLabelPrevious),
+    );
+  }
+
+  Widget _buildButtonNext(BuildContext context) {
+    return OutlinedButton(
+      onPressed: () {},
+      child: const Text(_buttonLabelNext),
+    );
+  }
+}
