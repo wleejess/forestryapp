ea150f3f03effa619bf0196b911ff32cd8897049
Jeremy Tsang <jeremytsang1@gmail.com>
2024-02-05 02:46:32 -0800

    Add some green to the app bar

--- lib/styles.dart
+++ lib/styles.dart
@@ -3,9 +3,15 @@ import 'package:flutter/material.dart';
 class Styles {
   static final _colorScheme = ColorScheme.fromSeed(seedColor: Colors.green);
 
+  static final _appBarTheme = AppBarTheme(
+    backgroundColor: _colorScheme.primaryContainer,
+    foregroundColor: _colorScheme.onPrimaryContainer,
+  );
+
   static ThemeData makeTheme() {
     return ThemeData(
       useMaterial3: true,
+      appBarTheme: _appBarTheme,
       colorScheme: _colorScheme,
     );
   }
