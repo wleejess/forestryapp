081abf7b363fa17df26113be2e9c56aa99c49e95
Jeremy Tsang <jeremytsang1@gmail.com>
2024-02-05 16:46:38 -0800

    Add contact info fields and save button

--- lib/screens/landowner_edit.dart
+++ lib/screens/landowner_edit.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:forestryapp/components/forestry_scaffold.dart';
+import 'package:forestryapp/components/person_fieldset.dart';
 
 class LandownerEdit extends StatefulWidget {
   const LandownerEdit({super.key});
@@ -11,6 +12,7 @@ class LandownerEdit extends StatefulWidget {
 class _LandownerEditState extends State<LandownerEdit> {
   // Static variables //////////////////////////////////////////////////////////
   static const _title = "Edit Landowner:";
+  static const _labelSaveButton = "Save";
 
   // Instance variables ////////////////////////////////////////////////////////
   final _formKey = GlobalKey<FormState>();
@@ -22,7 +24,24 @@ class _LandownerEditState extends State<LandownerEdit> {
       title: _title,
       body: Form(
         key: _formKey,
-        child: const Placeholder(),
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              const PersonFieldSet(),
+              _buildSaveButton(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Align _buildSaveButton() {
+    return Align(
+      alignment: Alignment.bottomRight,
+      child: OutlinedButton(
+        onPressed: () {},
+        child: const Text(_labelSaveButton),
       ),
     );
   }
