d101b0ff1032d7d6ba62424fb30487df206470dd
Jessica Lee <wlee.jessica@gmail.com>
2024-02-07 11:32:58 -0800

    Added helper functions to make code easier to read

--- lib/screens/site_characteristics.dart
+++ lib/screens/site_characteristics.dart
@@ -7,11 +7,6 @@ import "package:forestryapp/enums/slope_position.dart";
 class SiteCharacteristics extends StatelessWidget {
   // Static Variables
   static const _title = "Site Characteristics";
-  final TextEditingController _elevationController = TextEditingController();
-  final TextEditingController _aspectController = TextEditingController();
-  final TextEditingController _slopePercentageController =
-      TextEditingController();
-  final TextEditingController _soilInfoController = TextEditingController();
 
   SiteCharacteristics({super.key});
 
@@ -30,65 +25,69 @@ class SiteCharacteristics extends StatelessWidget {
                 'General Information',
                 style: TextStyle(fontWeight: FontWeight.bold),
               ),
-
-              // Elevation, Aspect, % Slope
-              Wrap(
-                spacing: 10.0,
-                runSpacing: 10.0,
-                children: [
-                  FreeTextBox(
-                    labelText: 'Elevation',
-                    controller: _elevationController,
-                    helperText: '',
-                    onChanged: (text) {
-                      // Handle elevation text changes
-                    },
-                  ),
-                  FreeTextBox(
-                    labelText: 'Aspect',
-                    controller: _aspectController,
-                    helperText: '',
-                    onChanged: (text) {
-                      // Handle aspect text changes
-                    },
-                  ),
-                  FreeTextBox(
-                    labelText: '% Slope',
-                    controller: _slopePercentageController,
-                    helperText: '',
-                    onChanged: (text) {},
-                  ),
-                ],
-              ),
-
-              const SizedBox(height: 16.0),
-
-              // Slope Position
-              RadioOptions(
-                header: 'Slope Position:',
-                enumValues: SlopePosition.values,
-                initialValue: SlopePosition.lower,
-                onSelected: (selectedOption) {
-                  // Handle slope position selection
-                },
-              ),
-
-              const SizedBox(height: 16.0),
-
-              // Soil Information
-              FreeTextBox(
-                labelText: 'Soil Information',
-                controller: _soilInfoController,
-                helperText:
-                    'Add any information about the soils that is available to you from either the landowner or obtain it online and add this information after your visit.',
-                onChanged: (text) {
-                  // Handle soil information text changes
-                },
-              ),
+              _buildElevationAspectSlope(context),
+              _buildSlopePosition(context),
+              _buildSoilInformation(context)
             ],
           ),
         ),
       ),
     );
   }
+}
+
+Widget _buildElevationAspectSlope(BuildContext context) {
+  final TextEditingController _elevationController = TextEditingController();
+  final TextEditingController _aspectController = TextEditingController();
+  final TextEditingController _slopePercentageController =
+      TextEditingController();
+  return Wrap(spacing: 10.0, runSpacing: 10.0, children: [
+    FreeTextBox(
+      labelText: 'Elevation',
+      controller: _elevationController,
+      helperText: '',
+      onChanged: (text) {
+        // Handle elevation text changes
+      },
+    ),
+    FreeTextBox(
+      labelText: 'Aspect',
+      controller: _aspectController,
+      helperText: '',
+      onChanged: (text) {
+        // Handle aspect text changes
+      },
+    ),
+    FreeTextBox(
+      labelText: '% Slope',
+      controller: _slopePercentageController,
+      helperText: '',
+      onChanged: (text) {},
+    ),
+    const SizedBox(height: 16.0)
+  ]);
+}
+
+Widget _buildSlopePosition(BuildContext context) {
+  return RadioOptions(
+    header: 'Slope Position:',
+    enumValues: SlopePosition.values,
+    initialValue: SlopePosition.lower,
+    onSelected: (selectedOption) {
+      // Handle slope position selection
+    },
+  );
+}
+
+Widget _buildSoilInformation(BuildContext context) {
+  final TextEditingController _soilInfoController = TextEditingController();
+  return FreeTextBox(
+    labelText: 'Soil Information',
+    controller: _soilInfoController,
+    helperText:
+        'Add any information about the soils that is available to you from either the landowner or obtain it online and add this information after your visit.',
+    onChanged: (text) {
+      // Handle soil information text changes
+    },
+  );
 }
