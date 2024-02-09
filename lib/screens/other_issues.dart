b9983214b133a878ecfd7ac52a07dd9088385e5a
Anastasia Sledgianowski <ani.sledge@gmail.com>
2024-02-09 16:15:50 -0700

    Update the other issues form to use the FormScaffold

--- lib/screens/other_issues.dart
+++ lib/screens/other_issues.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:forestryapp/components/forestry_scaffold.dart';
+import 'package:forestryapp/components/form_scaffold.dart';
+import 'package:forestryapp/components/free_text.dart';
 
 class OtherIssues extends StatelessWidget {
-  final TextEditingController _textEditingController = TextEditingController();
+  final TextEditingController _otherController = TextEditingController();
   static const _title = "Other Issues";
   static const _otherDescription =
       "Describe any other health related issues you observed.";
-  static const _buttonLabelPrevious = "Previous";
-  static const _buttonLabelNext = "Next";
 
   OtherIssues({super.key});
 
@@ -16,67 +16,21 @@ class OtherIssues extends StatelessWidget {
   Widget build(BuildContext context) {
     return ForestryScaffold(
       title: _title,
-      body: Column(
+      body: FormScaffold(
         children: <Widget>[
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: _buildDescription(context),
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              _buildButtonPrevious(context),
-              _buildButtonNext(context),
-            ],
-          ),
+          _buildDescription(context)
         ],
-      ),
+      )
     );
   }
 
   // Inputs ////////////////////////////////////////////////////////////////////
-  /// Builds a TextField with a label, helper text, and an outline.
-  Widget _buildTextInput(BuildContext context, label, helper) {
-    return TextField(
-      controller: _textEditingController,
-      maxLines:
-          null, // Setting this to null allows the TextField to expand as needed
-      keyboardType: TextInputType.multiline, // Specify the keyboard type
-      decoration: InputDecoration(
-        labelText: label,
-        border: const OutlineInputBorder(),
-      ),
-    );
-  }
-
   Widget _buildDescription(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        _buildTextInput(context, _title, _otherDescription),
-        const SizedBox(height: 16.0),
-        const Text(
-          _otherDescription,
-          style: TextStyle(fontSize: 14.0, fontStyle: FontStyle.italic),
-        ),
-      ],
-    );
-  }
-
-  // Buttons ///////////////////////////////////////////////////////////////////
-  Widget _buildButtonPrevious(BuildContext context) {
-    return OutlinedButton(
-      onPressed: () {},
-      child: const Text(_buttonLabelPrevious),
-    );
-  }
-
-  Widget _buildButtonNext(BuildContext context) {
-    return OutlinedButton(
-      onPressed: () {},
-      child: const Text(_buttonLabelNext),
+    return FreeTextBox(
+      controller: _otherController, 
+      labelText: _title,
+      helperText: _otherDescription,
+      onChanged: (text) {}
     );
   }
 }
