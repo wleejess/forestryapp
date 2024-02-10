3a72ba9af3996febca5b84837eba491dddc56b68
Jeremy Tsang <jeremytsang1@gmail.com>
2024-02-09 17:19:59 -0800

    Instantiate singleton `SharedPreferences`
    Since Shared Preferences is a singleton keep a single reference to it in
    order to allow it to manage all the async code instead of littering the
    codebase with `async` and `await`. Use an inherited widget to wrap the
    entire `MaterialApp` in order to allow it to be accessed from anywhere
    in the tree since in the futures font size setting will likely be need
    to be available from each screen.

--- lib/forestryapp.dart
+++ lib/forestryapp.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:forestryapp/dependency_injection/inherited_preferences.dart';
 import 'package:forestryapp/screens/landowner_index.dart';
 import 'package:forestryapp/styles.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ForestryApp extends StatelessWidget {
-  const ForestryApp({super.key});
+  // Instance variables  ///////////////////////////////////////////////////////
+  final SharedPreferences _sharedPreferences;
+
+  // Constructor ///////////////////////////////////////////////////////////////
+  const ForestryApp({
+    required SharedPreferences sharedPreferences,
+    super.key,
+  }) : _sharedPreferences = sharedPreferences;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Forestry Wellness Checkup App',
-      theme: Styles.makeTheme(),
-      // Use Landowner Index for home page until settings is implemented.
-      home: const LandownerIndex(),
+    return InheritedSharedPreferences(
+      sharedPreferences: _sharedPreferences,
+      child: MaterialApp(
+        title: 'Forestry Wellness Checkup App',
+        theme: Styles.makeTheme(),
+        // Use Landowner Index for home page until settings is implemented.
+        home: const LandownerIndex(),
+      ),
     );
   }
 }
