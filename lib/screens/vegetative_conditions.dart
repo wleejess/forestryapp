a2dd5faed07ee9f9397fa7a69507298d5b920aaf
Anastasia Sledgianowski <ani.sledge@gmail.com>
2024-02-10 23:39:46 -0700

    Update VegetativeConditions to be a StatefulWidget

--- lib/screens/vegetative_conditions.dart
+++ lib/screens/vegetative_conditions.dart
@@ -9,7 +9,7 @@ import "package:forestryapp/enums/stand_density.dart";
 import "package:forestryapp/enums/stand_structure.dart";
 import "package:forestryapp/enums/cover_type.dart";
 
-class VegetativeConditions extends StatelessWidget {
+class VegetativeConditions extends StatefulWidget {
   // Instance Variables
   static const _title = "Vegetative Conditions";
   static const _coverTitle = "Cover Type";
@@ -21,97 +21,123 @@ class VegetativeConditions extends StatelessWidget {
   const VegetativeConditions({super.key});
 
   @override
+  State<VegetativeConditions> createState() => _VegetativeConditionsState();
+}
+
+class _VegetativeConditionsState extends State<VegetativeConditions> {
+  // State /////////////////////////////////////////////////////////////////////
+  final _formKey = GlobalKey<FormState>();
+  late final TextEditingController _coverTypeController;
+  late final TextEditingController _standHistoryController;
+
+  // Lifecycle Methods /////////////////////////////////////////////////////////
+  @override
+  void initState() {
+    super.initState();
+
+    _coverTypeController = TextEditingController();
+    _standHistoryController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _coverTypeController.dispose();
+    _standHistoryController.dispose();
+
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return ForestryScaffold(
-      title: _title,
+      title: VegetativeConditions._title,
       body: FormScaffold(
+        formKey: _formKey,
         children: <Widget>[
           // Cover Type
-          _buildCoverType(context, _coverTitle),
+          _buildCoverType(context, VegetativeConditions._coverTitle),
           _buildOtherCoverType(context),
-          _buildStandStructure(context, _strucTitle),
+          _buildStandStructure(context, VegetativeConditions._strucTitle),
           // Overstory Stand Density
-          _buildStoryInfo(context, _overstoryInfo, _standDensity),
+          _buildStoryInfo(context, VegetativeConditions._overstoryInfo, VegetativeConditions._standDensity),
           const SizedBox(height: 16.0),
-          _buildStoryInfo(context, _understoryInfo, _standDensity),
+          _buildStoryInfo(context, VegetativeConditions._understoryInfo, VegetativeConditions._standDensity),
           _buildStandHistory(context)
         ],
       )
     );
   }
-}
 
-Widget _buildCoverType(BuildContext context, header) {
-  return PortraitHandlingSizedBox(
-    child: DropdownOptions(
+  Widget _buildCoverType(BuildContext context, header) {
+    return PortraitHandlingSizedBox(
+      child: DropdownOptions(
+        header: header,
+        enumValues: CoverType.values,
+        initialValue: CoverType.forest,
+        onSelected: (selectedOption) {},
+      ),
+    );
+  }
+
+  Widget _buildOtherCoverType(BuildContext context) {
+    return PortraitHandlingSizedBox(
+      child: FreeTextBox(
+        labelText: "Other Cover Type",
+        controller: _coverTypeController,
+        helperText: "List cover type if the option is not in the dropdown.",
+        onChanged: (text) {
+          // Handle elevation text changes
+        },
+      )
+    );
+  }
+
+  Widget _buildStandStructure(BuildContext context, header) {
+    return RadioOptions(
       header: header,
-      enumValues: CoverType.values,
-      initialValue: CoverType.forest,
-      onSelected: (selectedOption) {},
-    ),
-  );
-}
+      enumValues: StandStructure.values,
+      initialValue: StandStructure.evenAged,
+      onSelected: (selectedOption) {
+        // Handle slope position selection
+      },
+    );
+  }
 
-Widget _buildOtherCoverType(BuildContext context) {
-  final TextEditingController coverType = TextEditingController();
-  return PortraitHandlingSizedBox(
-    child: FreeTextBox(
-      labelText: "Other Cover Type",
-      controller: coverType,
-      helperText: "List cover type if the option is not in the dropdown.",
+  Widget _buildStoryInfo(BuildContext context, title, density) {
+    return ExpansionTile(
+      title: Text(title),
+      collapsedBackgroundColor: const Color.fromARGB(255, 46, 96, 69),
+      collapsedTextColor: Colors.white,
+      collapsedIconColor: Colors.white,
+      children: [
+        RadioOptions(
+          header: density,
+          enumValues: StandDensity.values,
+          initialValue: StandDensity.low,
+          onSelected: (selectedOption) {
+            // Handle overstory stand density selection
+          },
+        ),
+        const TextField(
+            decoration: InputDecoration(
+          labelText: "Species Composition",
+          hintText: "Enter a % value",
+        )),
+      ],
+    );
+  }
+
+  Widget _buildStandHistory(BuildContext context) {
+    const historyTitle = "Stand/Area History";
+    const historyHelp = "Describe prior management activities and/or disturbances"
+        "that have shaped or influenced the stand as it appears today";
+    return FreeTextBox(
+      labelText: historyTitle,
+      helperText: historyHelp,
+      controller: _standHistoryController,
       onChanged: (text) {
         // Handle elevation text changes
       },
-    )
-  );
-}
-
-Widget _buildStandStructure(BuildContext context, header) {
-  return RadioOptions(
-    header: header,
-    enumValues: StandStructure.values,
-    initialValue: StandStructure.evenAged,
-    onSelected: (selectedOption) {
-      // Handle slope position selection
-    },
-  );
-}
-
-Widget _buildStoryInfo(BuildContext context, title, density) {
-  return ExpansionTile(
-    title: Text(title),
-    collapsedBackgroundColor: const Color.fromARGB(255, 46, 96, 69),
-    collapsedTextColor: Colors.white,
-    collapsedIconColor: Colors.white,
-    children: [
-      RadioOptions(
-        header: density,
-        enumValues: StandDensity.values,
-        initialValue: StandDensity.low,
-        onSelected: (selectedOption) {
-          // Handle overstory stand density selection
-        },
-      ),
-      const TextField(
-          decoration: InputDecoration(
-        labelText: "Species Composition",
-        hintText: "Enter a % value",
-      )),
-    ],
-  );
-}
-
-Widget _buildStandHistory(BuildContext context) {
-  final TextEditingController history = TextEditingController();
-  const historyTitle = "Stand/Area History";
-  const historyHelp = "Describe prior management activities and/or disturbances"
-      "that have shaped or influenced the stand as it appears today";
-  return FreeTextBox(
-    labelText: historyTitle,
-    helperText: historyHelp,
-    controller: history,
-    onChanged: (text) {
-      // Handle elevation text changes
-    },
-  );
+    );
+  }
 }
