168be7bee583c3efc88d138ac40ca50ea1edd1ba
Anastasia Sledgianowski <ani.sledge@gmail.com>
2024-02-10 23:15:41 -0700

    Update OtherIssues to be a Stateful widget

--- lib/screens/other_issues.dart
+++ lib/screens/other_issues.dart
@@ -3,20 +3,44 @@ import 'package:forestryapp/components/forestry_scaffold.dart';
 import 'package:forestryapp/components/form_scaffold.dart';
 import 'package:forestryapp/components/free_text.dart';
 
-class OtherIssues extends StatelessWidget {
-  final TextEditingController _otherController = TextEditingController();
+class OtherIssues extends StatefulWidget {
   static const _title = "Other Issues";
   static const _otherDescription =
       "Describe any other health related issues you observed.";
 
-  OtherIssues({super.key});
+  const OtherIssues({super.key});
+
+  @override
+  State<OtherIssues> createState() => _OtherIssuesState();
+}
+
+class _OtherIssuesState extends State<OtherIssues> {
+  // State /////////////////////////////////////////////////////////////////////
+  final _formKey = GlobalKey<FormState>();
+  late final TextEditingController _otherController;
+
+  // Lifecycle Methods /////////////////////////////////////////////////////////
+  @override
+  void initState() {
+    super.initState();
+
+    _otherController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _otherController.dispose();
+
+    super.dispose();
+  }
 
   // Methods ///////////////////////////////////////////////////////////////////
   @override
   Widget build(BuildContext context) {
     return ForestryScaffold(
-      title: _title,
+      title: OtherIssues._title,
       body: FormScaffold(
+        formKey: _formKey,
         children: <Widget>[
           _buildDescription(context)
         ],
@@ -28,8 +52,8 @@ class OtherIssues extends StatelessWidget {
   Widget _buildDescription(BuildContext context) {
     return FreeTextBox(
       controller: _otherController, 
-      labelText: _title,
-      helperText: _otherDescription,
+      labelText: OtherIssues._title,
+      helperText: OtherIssues._otherDescription,
       onChanged: (text) {}
     );
   }
