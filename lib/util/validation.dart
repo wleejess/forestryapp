bcf4c2a296cbba1d7b780c1ecf9ba6fd489fa74c
Anastasia Sledgianowski <ani.sledge@gmail.com>
2024-02-16 13:10:04 -0700

    Add the validation class from the form-validation-routeobserver branch

---
+++ lib/util/validation.dart
@@ -0,0 +1,30 @@
+class Validation {
+  static String? isNotEmpty(value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter a response.';
+    }
+    return null;
+  }
+
+  static String? isValidPercentage(value) {
+    if (value != null && value.isNotEmpty) {
+      int number = int.parse(value);
+
+      if (number >= 0 && number <= 100) {
+        return null;
+      }
+    }
+    return 'Please enter a valid percentage.';
+  }
+
+  static String? isValidElevation(value) {
+    if (value != null && value.isNotEmpty) {
+      int number = int.parse(value);
+
+      if (number >= -5000 && number <= 50000) {
+        return null;
+      }
+    }
+    return 'Please enter an elevation from -5000 to 50000.';
+  }
+}
