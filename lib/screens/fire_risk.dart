160876e5914ff885e88c90093999109bbde2f82b
Jessica Lee <wlee.jessica@gmail.com>
2024-02-15 17:32:47 -0800

    Updated screens to use provider

--- lib/screens/fire_risk.dart
+++ lib/screens/fire_risk.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:forestryapp/components/forestry_scaffold.dart';
 import 'package:forestryapp/components/form_scaffold.dart';
 import 'package:forestryapp/components/free_text.dart';
+import 'package:provider/provider.dart';
+import 'package:forestryapp/models/fire_risk_data.dart';
 
 class FireRisk extends StatelessWidget {
   static const _title = "Fire Risk";
@@ -18,22 +20,22 @@ class FireRisk extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ForestryScaffold(
-      title: FireRisk._title,
-      body: FormScaffold(
-        formKey: _formKey,
-        children: <Widget>[
-          _buildDescription(context)
-        ],
-      )
-    );
+        title: FireRisk._title,
+        body: FormScaffold(
+          formKey: _formKey,
+          children: <Widget>[_buildDescription(context)],
+        ));
   }
 
   // Inputs ////////////////////////////////////////////////////////////////////
   Widget _buildDescription(BuildContext context) {
+    final fireRiskData = Provider.of<FireRiskDataModel>(context);
+
     return FreeTextBox(
-      labelText: FireRisk._title,
-      helperText: FireRisk._fireDescription,
-      onChanged: (text) {}
-    );
+        labelText: FireRisk._title,
+        helperText: FireRisk._fireDescription,
+        onChanged: (text) {
+          fireRiskData.fireInfo = text;
+        });
   }
 }
