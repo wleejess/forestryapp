8196a5a9bbfb6bc039d0e235b17c8b61d007bf76
Jeremy Tsang <jeremytsang1@gmail.com>
2024-02-16 15:38:15 -0800

    Change dummy data to single expression
    Do this because sqflite only reads the first expression of the file.

--- assets/database/dummy_data.sql
+++ assets/database/dummy_data.sql
@@ -1,13 +1,6 @@
 -- Landowners -----------------------------------------------------------------
-INSERT INTO landowners(name, address, city, us_state, zip) VALUES(
-  'Landowner A', 'Address A', 'City A', 'al', '11111'
-);
-INSERT INTO landowners(name, address, city, us_state, zip) VALUES(
-  'Landowner B', 'Address B', 'City B', 'ak', '22222'
-);
-INSERT INTO landowners(name, address, city, us_state, zip) VALUES(
-  'Landowner C', 'Address C', 'City C', 'as', '33333'
-);
-INSERT INTO landowners(name, address, city, us_state, zip) VALUES(
-  'Landowner D', 'Address D', 'City D', 'az', '44444'
-);
+INSERT INTO landowners(name, address, city, us_state, zip) VALUES
+('Landowner A', 'Address A', 'City A', 'al', '11111'),
+('Landowner B', 'Address B', 'City B', 'ak', '22222'),
+('Landowner C', 'Address C', 'City C', 'as', '33333'),
+('Landowner D', 'Address D', 'City D', 'az', '44444');
