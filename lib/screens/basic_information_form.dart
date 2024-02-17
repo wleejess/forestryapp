1fb91580310e542495a82a4034c767cbdfef4473
Jeremy Tsang <jeremytsang1@gmail.com>
2024-02-17 02:16:08 -0800

    Replace dummy data with live data from database

--- lib/screens/basic_information_form.dart
+++ lib/screens/basic_information_form.dart
@@ -1,6 +1,6 @@
 import "package:flutter/material.dart";
-import "package:forestryapp/enums/us_state.dart";
 import "package:forestryapp/models/landowner.dart";
+import "package:forestryapp/models/landowner_collection.dart";
 import 'package:provider/provider.dart';
 import "package:forestryapp/components/forestry_scaffold.dart";
 import "package:forestryapp/components/form_scaffold.dart";
@@ -92,13 +92,7 @@ class BasicInformationForm extends StatelessWidget {
   Widget _buildLandownerInput(BuildContext context) {
     final basicInfoData = Provider.of<BasicInformation>(context);
 
-    List<Landowner> landownerOptions = <Landowner>[
-      Landowner(id: 0, name: "Amy Adams", email: "a@gmail.com", address: "1234 Alpha Street", city: "Acton", state: USState.alabama, zip: "1"),
-      Landowner(id: 1, name: "Bob Bancroft", email: "b@gmail.com", address: "1234 Beta Street", city: "Burne", state: USState.arizona, zip: "2"),
-      Landowner(id: 2, name: "Chet Chapman", email: "c@gmail.com", address: "1234 Gamma Street", city: "Chico", state: USState.california, zip: "3"),
-      Landowner(id: 3, name: "Donna Dawson", email: "d@gmail.com", address: "1234 Delta Street", city: "Davis", state: USState.delaware, zip: "4"),
-      Landowner(id: 4, name: "Edgar Edmonds", email: "e@gmail.com", address: "1234 Epsilon Street", city: "Empire", state: USState.oregon, zip: "5"),
-    ];
+    List<Landowner> landownerOptions = Provider.of<LandownerCollection>(context).landowners;
 
     Landowner? getInitialValue() {
       if (basicInfoData.landowner != null) {
