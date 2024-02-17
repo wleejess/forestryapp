73057450089fa4687f36bca6ae5bc6b12b17e236
Jeremy Tsang <jeremytsang1@gmail.com>
2024-02-16 16:52:25 -0800

    Convert `LandownerIndex` back to stateless
    Don't make it fetch anything. Instead have it rebuild its `ListView`
    when the collection of landowners is updated.

--- lib/screens/landowner_index.dart
+++ lib/screens/landowner_index.dart
@@ -2,64 +2,54 @@ import "package:flutter/material.dart";
 import "package:forestryapp/components/fab_creation.dart";
 import "package:forestryapp/components/forestry_scaffold.dart";
 import "package:forestryapp/components/navigable_list_tile.dart";
-import "package:forestryapp/database/dao_landowner.dart";
-import "package:forestryapp/models/landowner.dart";
+import "package:forestryapp/models/landowner_collection.dart";
 import "package:forestryapp/screens/landowner_review.dart";
+import "package:provider/provider.dart";
 
 /// Screen for showing saved landowners from the navigation drawer.
 ///
 /// Can view existing landowners or create new landowners from this screen.
-class LandownerIndex extends StatefulWidget {
+class LandownerIndex extends StatelessWidget {
   static const _title = "Landowners";
 
   // Constructors //////////////////////////////////////////////////////////////
   const LandownerIndex({super.key});
-
-  @override
-  State<LandownerIndex> createState() => _LandownerIndexState();
-}
-
-class _LandownerIndexState extends State<LandownerIndex> {
-  late List<Landowner> _landowners = [];
-
-  // Lifecycle Methods /////////////////////////////////////////////////////////
-  @override
-  void initState() {
-    super.initState();
-
-    // `initState()` itself can't be `async` but it can call `async` functions.
-    _readLandowners();
-  }
-
-  void _readLandowners() async {
-    _landowners = await DAOLandowner.fetchFromDatabase();
-    setState(() {});
-  }
 
   // Methods ///////////////////////////////////////////////////////////////////
   @override
   Widget build(BuildContext context) {
     return ForestryScaffold(
       title: LandownerIndex._title,
-      body: ListView.builder(
-        itemCount: _landowners.length,
-        itemBuilder: _landownerIndexListTileBuilder,
+      body: ListenableBuilder(
+        listenable: Provider.of<LandownerCollection>(context),
+        builder: _buildListViewOfAllLandownersFromDB,
       ),
       fab: FABCreation(icon: Icons.person, onPressed: () {}),
     );
   }
 
+  ListView _buildListViewOfAllLandownersFromDB(
+    BuildContext context,
+    Widget? child,
+  ) {
+    return ListView.builder(
+      itemCount: Provider.of<LandownerCollection>(context).landowners.length,
+      itemBuilder: _landownerIndexListTileBuilder,
+    );
+  }
+
   Widget _landownerIndexListTileBuilder(BuildContext context, int i) {
-    final String name = _landowners[i].name;
+    final landowners = Provider.of<LandownerCollection>(context).landowners;
+    final String name = landowners[i].name;
     return NavigableListTile(
       titleText: name,
       routeBuilder: (context) => LandownerReview(
         name: name,
-        email: _landowners[i].email,
-        streetAddress: _landowners[i].address,
-        city: _landowners[i].city,
-        state: (_landowners[i].state == null) ? '': _landowners[i].state!.label,
-        zipCode: _landowners[i].zip,
+        email: landowners[i].email,
+        streetAddress: landowners[i].address,
+        city: landowners[i].city,
+        state: (landowners[i].state == null) ? '' : landowners[i].state!.label,
+        zipCode: landowners[i].zip,
         // TODO: The areas should actually be queried from `LandownerReview` and
         // this parameter should be removed entirely.
         areas: const [],
