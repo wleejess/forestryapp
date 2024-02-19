000e97b718e6fada484d0515772a6f60dffc3605
Jeremy Tsang <jeremytsang1@gmail.com>
2024-02-19 15:45:43 -0800

    Start Area Data Access Object (DAO) by adding column names

---
+++ lib/database/dao_area.dart
@@ -0,0 +1,28 @@
+class DAOArea {
+  // Static Variables //////////////////////////////////////////////////////////
+  // Column names from database when converted to Dart [Map] keys.
+  static const landownerId = 'landowner_id';
+  static const name = 'name';
+  static const acres = 'acres';
+  static const goals = 'goals';
+  static const elevation = 'elevation';
+  static const aspect = 'aspect';
+  static const slopePercentage = 'slope_percentage';
+  static const slopePosition = 'slope_position';
+  static const soilInformation = 'soil_information';
+  static const coverType = 'cover_type';
+  static const standStructure = 'stand_structure';
+  static const overstoryStandDensity = 'overstory_stand_density';
+  static const overstorySpeciesComposition = 'overstory_species_composition';
+  static const understoryStandDensity = 'understory_stand_density';
+  static const understorySpeciesComposition = 'understory_species_composition';
+  static const history = 'history';
+  static const wildlifeDamage = 'wildlife_damage';
+  static const mistletoeUniformity = 'mistletoe_uniformity';
+  static const mistletoeLocation = 'mistletoe_location';
+  static const hawksworth = 'hawksworth';
+  static const otherIssues = 'other_issues';
+  static const waterIssues = 'water_issues';
+  static const fireRisk = 'fire_risk';
+  static const diagnosis = 'diagnosis';
+}
