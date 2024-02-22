fc81fd774298e988f5829e100542254a3d5f81c4
Jessica Lee <wlee.jessica@gmail.com>
2024-02-21 19:13:17 -0800

    Created new file paths for 'Area' read/save

---
+++ assets/database/ddl_statements/save_new_area.sql
@@ -0,0 +1,10 @@
+INSERT INTO areas(name, acres, goals, elevation, aspect, slope_percentage, slope_position, 
+soil_information, cover_type, stand_structure, overstory_stand_density, overstory_species_composition, 
+understory_stand_density, understory_species_composition, history,
+wildlife_damage, mistletoe_uniformity, mistletoe_location,
+hawksworth, other_issues, water_issues, fire_risk, diagnosis) VALUES
+ (?, ?, ?, ?, ?, 
+  ?, ?, ?, ?, ?, 
+  ?, ?, ?, ?, ?, 
+  ?, ?, ?, ?, ?, 
+  ?, ?, ?);
