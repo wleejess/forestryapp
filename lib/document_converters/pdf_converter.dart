5c35f7cc22589b662a0a713315d11c7a29e7eb6f
Anastasia Sledgianowski <ani.sledge@gmail.com>
2024-02-23 16:17:44 -0700

    Add basic pdf template for pdf generation

--- lib/document_converters/pdf_converter.dart
+++ lib/document_converters/pdf_converter.dart
@@ -23,7 +23,7 @@ class PdfConverter {
     String? siteHistory,
     String? insects,
     String? diseases,
-    String? ivasives,
+    String? invasives,
     String? wildlifeDamage,
     String? mistletoeUniformity,
     String? mistletoeLocation,
@@ -33,7 +33,8 @@ class PdfConverter {
     String? waterHealth,
     String? fireRisk,
     String? otherIssues,
-    String? diagnosis,) {
+    String? diagnosis,
+    String? evaluator,) {
     final pdf = pdfWidget.Document();
     pdf.addPage(pdfWidget.Page(
       pageFormat: PdfPageFormat.a4,
@@ -41,10 +42,87 @@ class PdfConverter {
         return pdfWidget.Column(
           children: [
             pdfWidget.Text("Forest Wellness Checkup"),
+            pdfWidget.Text("Landowner name: $landowner"),
+            pdfWidget.Text("Address: "),
+            pdfWidget.Text("Email: "),
+            pdfWidget.Text("Stand/Area Name: $name"),
+            pdfWidget.Text("Acres: $acres"),
+            pdfWidget.Text("Landowner goals and objectives:"),
+            pdfWidget.Text("$goals"),
+            pdfWidget.Text("Site Characteristics"),
+            pdfWidget.Row(
+              children: [
+                pdfWidget.Text("Elevation: $elevation"),
+                pdfWidget.Text("Aspect: $aspect"),
+                pdfWidget.Text("% Slope: $slopePercentage %"),
+              ]
+            ),
+            // How to include checkmark options? 
+            pdfWidget.Text("Slope position: $slopePosition"),
+            pdfWidget.Text("Soil Information"),
+            pdfWidget.Text("$soilInfo"),
+            pdfWidget.Text("Vegetative Conditions"),
+            pdfWidget.Text("Cover type: $coverType"),
+            pdfWidget.Text("Stand structure: $standStructure"),
+            pdfWidget.Text("Overstory stand density: $overstoryDensity"),
+            pdfWidget.Text("Overstory species composition: $overstorySpeciesComposition %"),
+            pdfWidget.Text("Understory stand density: $understoryDensity"),
+            pdfWidget.Text("Understory species composition: $understorySpeciesComposition %"),
           ]
         );
       }
-    ));
+  ));
+  pdf.addPage(pdfWidget.Page(
+    pageFormat: PdfPageFormat.a4,
+    build: (pdfWidget.Context context) {
+        return pdfWidget.Column(
+          children: [
+            pdfWidget.Text("Stand/Area History:"),
+            pdfWidget.Text("$siteHistory"),
+            pdfWidget.Text("Pests & Damage"),
+            pdfWidget.Text("Insects Present (if known):"),
+            pdfWidget.Text("$insects"),
+            pdfWidget.Text("Diseases Present (if known):"),
+            pdfWidget.Text("$diseases"),
+            pdfWidget.Text("Invasive Plants & Animals:"),
+            pdfWidget.Text("$invasives"),
+            pdfWidget.Text("Wildlife Damage/Issues:"),
+            pdfWidget.Text("$wildlifeDamage"),
+            pdfWidget.Text("Mistletoe Infections:"),
+            pdfWidget.Text("Uniformity: $mistletoeUniformity"),
+            pdfWidget.Text("Mistletoe location: $mistletoeLocation"),
+            pdfWidget.Text("Hawksworth infection rating(0-6): $hawksworth"),
+            pdfWidget.Text("Tree species infected:"),
+            pdfWidget.Text("$mistletoeTreeSpecies"),
+            pdfWidget.Text("Road Health/Conditions:"),
+          ]
+        );
+      }
+    )
+  );
+  pdf.addPage(pdfWidget.Page(
+    pageFormat: PdfPageFormat.a4,
+    build: (pdfWidget.Context context) {
+        return pdfWidget.Column(
+          children: [
+            pdfWidget.Text("Road Health/Conditions:"),
+            pdfWidget.Text("$roadHealth"),
+            pdfWidget.Text("Water/Stream/Riparian Health & Issues:"),
+            pdfWidget.Text("$waterHealth"),
+            pdfWidget.Text("Fire Risk (fuel levels & ignition potential):"),
+            pdfWidget.Text("$fireRisk"),
+            pdfWidget.Text("Other issues (explain):"),
+            pdfWidget.Text("$otherIssues"),
+            pdfWidget.Text("Diagnosis & Suggestions"),
+            pdfWidget.Text("$diagnosis"),
+            pdfWidget.Text("Evaluator name: $evaluator"),
+            pdfWidget.Text("Email: "),
+            pdfWidget.Text("Address: "),
+          ]
+        );
+      }
+    )
+  );
     return pdf;
   }
 }
