3d7e8ba88e22adc2239e2702e63db5c57a197319
Jeremy Tsang <jeremytsang1@gmail.com>
2024-02-23 16:47:23 -0800

    Replace widget dummy data with database data in `AreaReview`
    Also extract the lengthy `ListView` to its own widget class to help
    unclutter `AreaReview`.

--- lib/screens/area_index.dart
+++ lib/screens/area_index.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:forestryapp/components/error_scaffold.dart";
 import "package:forestryapp/components/fab_creation.dart";
 import "package:forestryapp/components/forestry_scaffold.dart";
 import "package:forestryapp/components/navigable_list_tile.dart";
@@ -11,6 +12,10 @@ import "package:provider/provider.dart";
 ///
 /// Can view existing areas or create new areas from this screen.
 class AreaIndex extends StatelessWidget {
+  // Static Variables //////////////////////////////////////////////////////////
+  static const _errorTitle = "Area Not Found";
+  static const _errorBody = "Could not find area with id: ";
+
   // Instance variables ////////////////////////////////////////////////////////
   final _title = "Areas";
 
@@ -41,41 +46,20 @@ class AreaIndex extends StatelessWidget {
     );
   }
 
-  Widget _areaIndexListTileBuilder(BuildContext context, int i, List<Area> areas) {
+  Widget _areaIndexListTileBuilder(
+    BuildContext context,
+    int i,
+    List<Area> areas,
+  ) {
+    int? id = areas[i].id;
     return NavigableListTile(
       titleText: (areas[i].name)!,
-      routeBuilder: (context) => AreaReview(
-        name: (areas[i].name)!,
-         // TODO: Query the landowner on next page
-        landowner: (areas[i].landownerID == null) ? null : areas[i].landownerID.toString(),
-        acres: (areas[i].acres == null) ? null : areas[i].acres.toString(),
-        goals: areas[i].goals,
-        elevation: (areas[i].elevation == null) ? null : areas[i].elevation.toString(),
-        aspect: areas[i].aspect.label,
-        slopePercentage: (areas[i].slopePercentage == null) ? null : areas[i].slopePercentage.toString(),
-        slopePosition: areas[i].slopePosition.label,
-        soilInfo: areas[i].soilInfo,
-        coverType: areas[i].coverType.label,
-        standStructure: areas[i].standStructure.label,
-        overstoryDensity: areas[i].overstoryDensity.label,
-        overstorySpeciesComposition: (areas[i].overstorySpeciesComposition == null) ? null : areas[i].overstorySpeciesComposition.toString(),
-        understoryDensity: areas[i].understoryDensity.label,
-        understorySpeciesComposition: (areas[i].understorySpeciesComposition == null) ? null : areas[i].understorySpeciesComposition.toString(),
-        siteHistory: areas[i].standHistory,
-        insects: areas[i].insects,
-        diseases: areas[i].diseases,
-        ivasives: areas[i].invasives,
-        wildlifeDamage: areas[i].wildlifeDamage,
-        mistletoeUniformity: areas[i].mistletoeUniformity.label,
-        mistletoeLocation: areas[i].mistletoeLocation,
-        hawksworth: areas[i].hawksworth.label,
-        mistletoeTreeSpecies: areas[i].mistletoeTreeSpecies,
-        roadHealth: areas[i].roadHealth,
-        waterHealth: areas[i].waterHealth,
-        fireRisk: areas[i].fireRisk,
-        otherIssues: areas[i].otherIssues,
-        diagnosis: areas[i].diagnosis,
-      ),
+      routeBuilder: (context) {
+        if (id == null) {
+          return ErrorScaffold(title: _errorTitle, bodyText: "$_errorBody$id");
+        }
+        return AreaReview(id);
+      },
     );
   }
 }
