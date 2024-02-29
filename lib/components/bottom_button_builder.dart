adb032f3ed0d358d0542c297fc0d054f92e49988
Anastasia Sledgianowski <ani.sledge@gmail.com>
2024-02-29 16:42:22 -0700

    Remove unneeded imports and variables from bottom button builder

--- lib/components/bottom_button_builder.dart
+++ lib/components/bottom_button_builder.dart
@@ -1,22 +1,9 @@
 import 'package:flutter/material.dart';
 import "package:forestryapp/util/break_points.dart";
-import "package:forestryapp/database/dao_area.dart";
-import "package:forestryapp/document_converters/docx_converter.dart";
 import "package:forestryapp/models/area.dart";
-import "package:forestryapp/models/area_collection.dart";
-import "package:forestryapp/models/landowner_collection.dart";
-import "package:provider/provider.dart";
 
 /// Adjusts the button layout depending on the screen width.
 class BottomButtonBuilder {
-  // Static variables //////////////////////////////////////////////////////////
-  static const _buttonTextPDF = "Create PDF";
-  static const _buttonTextDOCX = "Create DOCX";
-  static const _buttonTextEdit = "Edit";
-  static const _buttonTextDelete = "Delete";
-  static const _buttonTextCancel = "Cancel";
-
-  // Instance variables //////////////////////////////////////////////////////////;
 
   Widget builder(
     BuildContext context,
