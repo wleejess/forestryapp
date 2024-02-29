1e409a0292a58048e575241e96c1a07bb5e90993
Anastasia Sledgianowski <ani.sledge@gmail.com>
2024-02-29 16:57:36 -0700

    Evenly divide the buttons into a table

--- lib/components/bottom_button_builder.dart
+++ lib/components/bottom_button_builder.dart
@@ -12,8 +12,27 @@ class BottomButtonBuilder {
     List<Widget> buttons,
   ) {
     if (constraints.maxWidth < BreakPoints.widthPhonePortait) {
-      return Wrap(
-        children: buttons
+      List<TableRow> rows = [];
+
+      // Each row must have an equal number of elements
+      if (buttons.length % 2 != 0) {
+        buttons.add(Container());
+      }
+
+      // Evenly divide the buttons into a 2-column table
+      for (var i = 0; i < buttons.length; i = i+2) {
+        rows.add(
+          TableRow(
+            children: [
+              buttons[i],
+              buttons[i+1]
+            ]
+          )
+        );
+      }
+
+      return Table(
+        children: rows,
       );
     }
 
