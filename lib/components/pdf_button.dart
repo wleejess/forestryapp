ed78a641c54c8e73a368b3e57e746367090a5ecf
Anastasia Sledgianowski <ani.sledge@gmail.com>
2024-03-05 23:24:51 -0700

    Use the ExceptionALert.alert function in PdfButton to catch file save exceptions

--- lib/components/pdf_button.dart
+++ lib/components/pdf_button.dart
@@ -1,5 +1,6 @@
 import "dart:io";
 import "package:flutter/material.dart";
+import "package:forestryapp/components/exception_alert.dart";
 import 'package:provider/provider.dart';
 import 'package:forestryapp/models/settings.dart';
 import "package:forestryapp/document_converters/pdf_converter.dart";
@@ -13,73 +14,78 @@ import "package:forestryapp/models/landowner.dart";
 class PdfButton extends StatelessWidget {
   // Static variables //////////////////////////////////////////////////////////
   static const _buttonText = "Create PDF";
+  static const _exceptionPrefix = "Exception (PdfButton)";
+  static const _errorNoLandowner = "Landowner not set";
+  static const _errorNoSettings = "Evaluator info not set.";
+  static const _errorNoName = "Area name not set.";
+  static const _errorNoDirectory = "Save directory not found.";
 
   // Instance variables ////////////////////////////////////////////////////////
-  final Area area;
-  final Landowner? landowner;
+  final Area _area;
+  final Landowner? _landowner;
 
   const PdfButton({
-    required this.area,
-    required this.landowner,
+    required area,
+    required landowner,
     super.key,
-  });
+  }) : _area = area,
+       _landowner = landowner;
 
   @override
   Widget build(BuildContext context) {
-    final evaluator = context.read<Settings>();
-
     return OutlinedButton(
-      onPressed: () async {
-        if (landowner != null) {
-          // Builds the PDF widget tree
-          final pdf = PdfConverter().create(area, landowner!, evaluator);
-
-          // Get the path of the folder in which to store the pdf file.
-          // On Android, you must access external storage in order to open the file outside of the app.
-          // https://stackoverflow.com/questions/63688285/flutter-platformexceptionerror-failed-to-find-configured-root-that-contains
-          Directory? directory;
-          if (Platform.isAndroid) {
-            directory = await getExternalStorageDirectory();
-          } else if (Platform.isIOS) {
-            directory = await getApplicationDocumentsDirectory();
-          } else {
-            directory = await getDownloadsDirectory();
-          }
-
-          if (directory != null) {
-            final file = File("${directory.absolute.path}/${area.name}.pdf");
-
-            try {
-              await file.writeAsBytes(await pdf.save());
-              OpenFile.open(file.path);
-
-            } catch(e) {
-              print(e);
-            }
-            
-          } else {
-            // Show an error message if the save directory cannot be found.
-            print("Cannot access directory to save PDF.");
-          }
-        } else {
-          // Users must enter a landowner before generating the PDF
-          print("Please select a landowner for the area.");
-        }
-      },
+      onPressed: () async => await _onPressed(context),
       child: const Text(_buttonText),
     );
   }
 
-  Widget _buildAlert (BuildContext context, dynamic error) {
-    return AlertDialog(
-      title: const Text('Error'),
-      content: Text(error),
-      actions: <Widget>[
-        TextButton(
-          onPressed: () => Navigator.pop(context, 'Cancel'), 
-          child: const Text('Cancel'),
-        )
-      ]
-    );
+  Future<void> _onPressed(BuildContext context) async {
+    // Don't build the PDF without an area title, landowner, and evaluator data.
+    if (_area.name == null) {
+      ExceptionAlert.alert(context: context, prefix: _exceptionPrefix, message: _errorNoName);
+    }
+
+    if (_landowner == null) {
+      ExceptionAlert.alert(context: context, prefix: _exceptionPrefix, message: _errorNoLandowner);
+      return;
+    }
+    
+    final evaluator = context.read<Settings>();
+    if (evaluator.evaluatorName.isEmpty) {
+      ExceptionAlert.alert(context: context, prefix: _exceptionPrefix, message: _errorNoSettings);
+    }
+
+    // Get the path of the folder in which to store the pdf file.
+    // On Android, you must access external storage in order to open the file outside of the app.
+    // https://stackoverflow.com/questions/63688285/flutter-platformexceptionerror-failed-to-find-configured-root-that-contains
+    Directory? directory;
+    if (Platform.isAndroid) {
+      directory = await getExternalStorageDirectory();
+    } else if (Platform.isIOS) {
+      directory = await getApplicationDocumentsDirectory();
+    } else {
+      directory = await getDownloadsDirectory();
+    }
+
+    if (directory == null) {
+      if (!context.mounted) return;
+      ExceptionAlert.alert(context: context, prefix: _exceptionPrefix, message: _errorNoDirectory);
+      
+    } else {
+      try {
+        // The path to the save folder
+        final file = File("${directory.absolute.path}/${_area.name}.pdf");
+
+        // Builds the PDF widget tree
+        final pdf = PdfConverter().create(_area, _landowner, evaluator);
+
+        await file.writeAsBytes(await pdf.save());
+        OpenFile.open(file.path);
+
+      } on FileSystemException catch (e) {
+        if (!context.mounted) return;
+        ExceptionAlert.alert(context: context, prefix: _exceptionPrefix, message: e.message, exception: e);
+      }
+    }
   }
 }
