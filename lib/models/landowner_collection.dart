wleejess/forestryapp

commit fb11ff0aa181de689537087797a86f4e020051f0

Change `LandownerCollection.getByID()` signature to allow `null`
Change it so that it returns `null` if given `null`.

lib/models/landowner_collection.dart
@@ -30,11 +30,23 @@ class LandownerCollection extends ChangeNotifier {
     notifyListeners();
   }
 
-  Landowner? getByID(int id) {
+  /// Search locally cached landowner models.
+  ///
+  /// Find landowner given its [id]. Return null if [id] is null or if no
+  /// such landowner with given [id] is found.
+  ///
+  /// Purpose is to minimize async code and extra calls to database which would
+  /// involve another conversion from database record to model object. Assumes
+  /// cache kept up to date with calls to refetch() on any database operation.
+  Landowner? getByID(int? id) {
+    if (id == null) return null; // No ID provided, then no landowner returned.
+
     final matches =
         _landowners.where((landowner) => landowner.id == id).toList();
 
-    return (matches.isNotEmpty) ? matches[0] : null;
+    if (matches.isEmpty) null;
+
+    return matches[0];
   }
 
   // Relationship Queries //////////////////////////////////////////////////////

commit bbb02a5ba87040c567f30448a3acc95bab60f415

FIX: Add missing return statement
This fixes the empty list exception

> RangeError (index): Invalid value: Valid value range is empty: 0

that was occurring when deleting a landowner in landowner review. This
fixes a bug introduced in fb11ff0.

lib/models/landowner_collection.dart
@@ -44,7 +44,7 @@ class LandownerCollection extends ChangeNotifier {
     final matches =
         _landowners.where((landowner) => landowner.id == id).toList();
 
-    if (matches.isEmpty) null;
+    if (matches.isEmpty) return null;
 
     return matches[0];
   }