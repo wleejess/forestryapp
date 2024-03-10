03d9bdea59962749562456f100e0d8c959d2a906
Anastasia Sledgianowski <ani.sledge@gmail.com>
2024-03-10 00:36:55 -0700

    Add instructions for the user to get started

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,7 +3,11 @@ import "package:forestryapp/components/forestry_scaffold.dart";
 
 class Home extends StatelessWidget {
   // Static variables //////////////////////////////////////////////////////////
-  static const _title = "Welcome to the Forest Wellness Checkup App";
+  static const _title = "Home";
+  static const _welcomeText = "Welcome to the Forest Wellness Checkup App!";
+  static const _stepOneText = "Step 1: Enter your contact information.";
+  static const _stepTwoText = "Step 2: Enter the landower's contact information.";
+  static const _stepThreeText = "Step 3: Fill out the checklist.";
 
   // Constructor ///////////////////////////////////////////////////////////////
   /// Creates a the home page / introduction screen.
@@ -14,11 +18,43 @@ class Home extends StatelessWidget {
   // Methods ///////////////////////////////////////////////////////////////////
   @override
   Widget build(BuildContext context) {
-    return const ForestryScaffold(
+    return ForestryScaffold(
       title: _title,
       body: Center(
-          child: Text("Hello."),
-      )
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(_welcomeText, style: Theme.of(context).textTheme.displayMedium),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Image(image: AssetImage('assets/images/brand.png')),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(_stepOneText, style: Theme.of(context).textTheme.displaySmall),
+              )),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(_stepTwoText, style: Theme.of(context).textTheme.displaySmall),
+              )),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(_stepThreeText, style: Theme.of(context).textTheme.displaySmall),
+              )),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
